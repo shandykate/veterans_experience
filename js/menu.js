97e91fd17d9c6d83f2819493a8b77c0b39a86c11
shandykate <shandy.kate@gmail.com>
2023-01-15 17:10:24 +0200

    Adds new header

--- js/menu.js
+++ js/menu.js
@@ -1,23 +1,10 @@
-(() => {
-    const menuBtnRef = document.querySelector("[data-menu-button]");
-    const mobileMenuRef = document.querySelector("[data-menu]");
-  
-    menuBtnRef.addEventListener("click", () => {
-      const expanded =
-        menuBtnRef.getAttribute("aria-expanded") === "true" || false;
-  
-      menuBtnRef.classList.toggle("is-open");
-      menuBtnRef.setAttribute("aria-expanded", !expanded);
-  
-      mobileMenuRef.classList.toggle("is-open");
-    });
-
-    // close menu on menu item click
-    document.querySelectorAll('.header--menu__link').forEach(item => {
-        item.addEventListener('click', event => {
-            mobileMenuRef.classList.toggle("is-open");
-            menuBtnRef.classList.toggle("is-open");
-        })
-    })
-
-  })();
+$(document).ready(function(){
+$('.header-burger').click(function(event){
+$('.header-burger, .header-menu').toggleClass('active');
+$('body').toggleClass('lock');
+});
+$('.header-link').click(function(event){
+    $('.header-burger, .header-menu').removeClass('active');
+    $('body').removeClass('lock');
+});
+});

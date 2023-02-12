05b369f7a53dd2ecc6bd6aca1a9482eb04a68a67
shandykate <shandy.kate@gmail.com>
2023-02-12 13:00:32 +0200

    Adds mob slick slider

--- js/slider.js
+++ js/slider.js
@@ -12,3 +12,18 @@ $(document).ready(function(){
     // fade:true,
     });
 });
+
+$(document).ready(function(){
+    $('.slider-mob').slick({
+    arrows:true,
+    dots:true,
+    adaptiveHeight:true,
+    speed:1000,
+    easing:'linear',
+    autoplay:true,
+    autoplaySpeed:2000,
+    pauseOnFocus:false,
+    pauseOnHover:false,
+    // fade:true,
+    });
+});
